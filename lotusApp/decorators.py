5226076635c4e5181f7c3726761c92c6b98d9fb1
LisanzaTabby <lisanzatabitha@gmail.com>
2024-10-23 19:27:00 +0300

    reviewed list loops

--- lotusApp/decorators.py
+++ lotusApp/decorators.py
@@ -2,14 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 
 def unauthenticated_user(view_func):
-        def _wrapped_view(request, *args, **kwargs):
-            if not request.user.is_authenticated:
-                return redirect('login')  
-            response = view_func(request, *args, **kwargs)
-            if response is None:
-                raise ValueError("View function returned None instead of an HttpResponse.")
-            return response
-        return _wrapped_view
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('dataentry')
+        else:
+            return view_func(request, *args, **kwargs)
+    return wrapper
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper(request, *args, **kwargs):
